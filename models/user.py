6ceebf48a4485a99f5c9777c6f4ab9dfa87c0274
towasme <towasm@gmail.com>
2020-04-01 15:00:40 -0500

    added database functionalities and attributes

--- models/user.py
+++ models/user.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +15,18 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    __tablename__ = "users"
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="users",
+                              cascade="all, delete-orphan")
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'file':
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
