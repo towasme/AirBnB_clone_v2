cbf40a5d9f48150632d81398608b4c317b589e07
darkares23 <desarrollojuseca@gmail.com>
2020-04-01 19:36:03 -0500

    changed cascaderelationship

--- models/place.py
+++ models/place.py
@@ -31,7 +31,7 @@ class Place(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
-                              cascade="delete")
+                              cascade="all, delete-orphan")
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
