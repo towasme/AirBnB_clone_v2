d68fbb1c7a446899a967fc7f12d91701a431f0f6
towasme <towasm@gmail.com>
2020-04-21 21:43:30 -0500

    fixed import state

--- web_flask/7-sates_list.py
+++ web_flask/7-sates_list.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from models import State
+from models.state import State
 
 
 app = Flask(__name__)
@@ -17,7 +17,6 @@ def storage_close(self):
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-    list_s = ""
     list_s = storage.all('State')
     return render_template('7-states_list.html', list_s=list_s)
 
