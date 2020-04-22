298eea9705b07c09e071f72b319d2999bb99e35a
towasme <towasm@gmail.com>
2020-04-22 17:04:05 -0500

    roll back to previous code

--- models/state.py
+++ models/state.py
@@ -16,14 +16,13 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
 
+    name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
     else:
-        name = ""
-        
+
         @property
         def cities(self):
             city_list = []
