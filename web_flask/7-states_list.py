7726dd2dc7eaa0181c75b6396524994f5a180ad7
towasme <towasm@gmail.com>
2020-04-22 17:07:02 -0500

    added new method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Write a script that starts a Flask web application """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
@@ -26,4 +25,4 @@ def close_storage(self):
 
 if __name__ == '__main__':
     """ Web app must be listening on 0.0.0.0 port 5000 """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
