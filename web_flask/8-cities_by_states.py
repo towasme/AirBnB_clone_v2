a911cd21deb86f95bed2a844c61f0b0bfba10fdb
towasme <towasm@gmail.com>
2020-04-22 20:51:44 -0500

    fixed the relationship when calling the cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def print_city_list():
     """ display html """
-    list_of_city = storage.all('City')
+    list_of_city = storage.all('State')
     return render_template('8-cities_by_states.html',
                            list_of_city=list_of_city)
 
